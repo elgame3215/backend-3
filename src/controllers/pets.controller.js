036368aeb174e598443b5b97aadbf04f1f5d458e
Franco <francofumiere321@gmail.com>
2025-03-18 20:55:28 -0300

    style/added break lines

--- src/controllers/pets.controller.js
+++ src/controllers/pets.controller.js
@@ -9,9 +9,12 @@ const getAllPets = async (req, res) => {
 
 const createPet = async (req, res) => {
   const { name, specie, birthDate } = req.body
+
   if (!name || !specie || !birthDate) return res.status(400).send({ status: 'error', error: 'Incomplete values' })
+
   const pet = PetDTO.getPetInputFrom({ name, specie, birthDate })
   const result = await petsService.create(pet)
+
   res.send({ status: 'success', payload: result })
 }
 
@@ -19,28 +22,36 @@ const updatePet = async (req, res) => {
   const petUpdateBody = req.body
   const petId = req.params.pid
   await petsService.update(petId, petUpdateBody)
+
   res.send({ status: 'success', message: 'pet updated' })
 }
 
 const deletePet = async (req, res) => {
   const petId = req.params.pid
   await petsService.delete(petId)
+
   res.send({ status: 'success', message: 'pet deleted' })
 }
 
 const createPetWithImage = async (req, res) => {
   const file = req.file
   const { name, specie, birthDate } = req.body
+
   if (!name || !specie || !birthDate) return res.status(400).send({ status: 'error', error: 'Incomplete values' })
+
   console.log(file)
+
   const pet = PetDTO.getPetInputFrom({
     name,
     specie,
     birthDate,
     image: `${__dirname}/../public/img/${file.filename}`
   })
+
   console.log(pet)
+
   const result = await petsService.create(pet)
+
   res.send({ status: 'success', payload: result })
 }
 export default {
